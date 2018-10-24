708e88a0c21b9a2ed4c0ea571bf7e7b589fec8a3
andresg6 <ajcapanzana@gmail.com>
2018-10-23 21:25:31 -0700

    end of class

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 let express = require("express");
+let path = require("path");
 let app = express();
 
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
 
 let characters = [
     {
@@ -26,15 +30,51 @@ let characters = [
         strengthPoints: 500
     }];
 
+app.get("/", function(req,res){
+    res.sendFile(path.join(_dirname, "view.html"));
+});
+
+app.get("/add", function (req, res){
+    res.sendFile(path.join(_dirname, "add.html"));
+});
+
 app.get("/", function (req, res) {
     res.send("Welcome to Avengers page!");
 });
 
-app.listen(PORT, function () {
-    console.log("Avengers assemble  on PORT" + PORT);
-});
+app.get("/", function(req, res) {
+    res.sendFile("view.html");
+})
 
 app.get("/api/v1/characters", function (req, res) {
     return res.json(characters);
 
+});
+
+app.get("/api/v1/characters/:characterId", function (req, res) {
+    let characterId = req.params.characterId;
+
+    console.log(characterId);
+
+    for (let i = 0; i < characters.length; i++) {
+        if (characterId === characters[i].routeName) {
+            return res.json(characters[i]);
+        }
+    }
+
+    return res.json(false + " Not a hero option");
+});
+
+app.post("/api/v1/characters", function (req, res) {
+    let newCharacter = req.body;
+
+    console.log(newCharacter);
+
+    characters.push(newCharacter);
+
+    return res.json(newCharacter);
+});
+
+app.listen(PORT, function () {
+    console.log("Avengers assemble  on PORT" + PORT);
 });
